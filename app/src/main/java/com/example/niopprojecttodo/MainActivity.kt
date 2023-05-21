917ecd32a9c1abbe43c212f714e66d35fc631a86
Flovrii <85340166+Flovrii@users.noreply.github.com>
2023-05-21 21:12:13 +0200

    asd

--- app/src/main/java/com/example/niopprojecttodo/MainActivity.kt
+++ app/src/main/java/com/example/niopprojecttodo/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.niopprojecttodo
 
+import android.content.Context
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.niopprojecttodo.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity(), TaskItemClickListener
 {
@@ -13,6 +17,23 @@ class MainActivity : AppCompatActivity(), TaskItemClickListener
     //private lateinit var taskViewModel: TaskViewModel
     private val taskViewModel: TaskViewModel by viewModels {
         TaskItemModelFactory((application as TodoApplication).repository)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    fun changeLanguage(context: Context, language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val res = context.resources
+        val config = Configuration(res.configuration)
+        config.setLocale(locale)
+        context.createConfigurationContext(config)
+        res.updateConfiguration(config, res.displayMetrics)
     }
 
     override fun onCreate(savedInstanceState: Bundle?)
